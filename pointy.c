15cfc2e4e140ec508a3c48a8b4bd663d06370b41
Adil Gondal <agondal@stuy.edu>
2018-09-17 20:17:50 -0400

    did #5 on hw

--- pointy.c
+++ pointy.c
@@ -2,16 +2,32 @@
 
 int main(){
   unsigned int x = 4205067000;
+  unsigned int b = 4205067000;
   char * c = &x;
-
+  char * d = &b;
   //printf("%d\n",sizeof(x));
   //printf("%d\n",sizeof(c));
 
   printf("int in hex:%x\n",x); //faa43af8
   
+  int j=0;
+  for (int j;j<sizeof(x);j++){
+    printf("%hhx\n",*(c+j));
+  }
+
   int i=0;
   for (int i;i<sizeof(x);i++){
-    printf("%hhx\n",*(c+i));
+    *c+=1;
+    printf("byte %d: %hhx\n",i,*c);
+    c++;
+  }
+  
+  c-=4;
+  int l=0;
+  for (int l;l<sizeof(x);l++){
+    *c+=16;
+    printf("byte %d: %hhx\n",l,*c);
+    c++;
   }
   
   return 0;
